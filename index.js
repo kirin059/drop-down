69284399dc0f6756c68e3739cffd3d19806a081f
saebom <boma91@gmail.com>
2021-03-19 00:00:27 +0900

    Add: js 생성

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 
 const toggle = document.querySelector(".dropdown-toggle");
 const dropdown = document.querySelector(".dropdown-lists");
-const
+const lists = document.querySelectorAll("li")
+const option = document.querySelectorAll("dropdown-option");
